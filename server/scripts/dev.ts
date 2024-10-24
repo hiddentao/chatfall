e625dfbc4151c4ef385f15d0cab58a6d8ecbef27
Ramesh Nair <ram@hiddentao.com>
2024-10-24 12:01:00 +0700

    fix: build client files first to avoid race conditions in further build

--- server/scripts/dev.ts
+++ server/scripts/dev.ts
@@ -1,26 +1,15 @@
 import { execa } from "execa"
 
 async function runDevCommand() {
-  const serverFrontendBuildProcess = execa(
-    "bun",
-    ["scripts/build-server-frontend.js", "--watch"],
-    {
-      env: { NODE_ENV: "development" },
-      stdio: "inherit",
-    },
-  )
+  await execa("bun", ["scripts/build-server-frontend.js", "--watch"], {
+    env: { NODE_ENV: "development" },
+    stdio: "inherit",
+  })
 
-  const serverProcess = execa(
-    "bun",
-    ["--hot", "--watch", "src/index.ts", "server"],
-    {
-      env: { NODE_ENV: "development" },
-      stdio: "inherit",
-    },
-  )
-
-  await serverFrontendBuildProcess
-  await serverProcess
+  await execa("bun", ["--hot", "--watch", "src/index.ts", "server"], {
+    env: { NODE_ENV: "development" },
+    stdio: "inherit",
+  })
 }
 
 runDevCommand().catch(console.error)
